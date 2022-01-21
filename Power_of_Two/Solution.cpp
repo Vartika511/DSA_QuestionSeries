a9c70c225601146644c2ab0d69fa548d6f6d42cf
Vartika Yaduvanshi <vv1415@srmist.edu.in>
2022-01-21 23:58:48 +0530

    Solution

---
+++ Power_of_Two/Solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        for(int i=0; i<31;i++){
+            int ans = pow(2,i);
+            if(ans==n){
+                return true;
+            }
+        }
+        return false;
+    }
+};
