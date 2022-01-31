9f80550c985c8344cfd9835c5c726cb65ea2beda
Vartika Yaduvanshi <vv1415@srmist.edu.in>
2022-01-31 19:06:44 +0530

    Solution

---
+++ Bubble Sort/Solution.cpp
@@ -0,0 +1,10 @@
+void bubbleSort(vector<int>& arr, int n)
+{   
+for(int i=0; i<n-1;i++){
+    for(int j=0; j<n-i-1;j++){
+        if (arr[j]>arr[j+1]){
+            swap(arr[j], arr[j+1]);
+        }
+    }
+}
+}
