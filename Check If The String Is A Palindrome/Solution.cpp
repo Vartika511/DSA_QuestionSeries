e7f778b739bcea2480e236928e8e448aeee5901f
Vartika Yaduvanshi <vv1415@srmist.edu.in>
2022-02-04 22:23:30 +0530

    Solution

---
+++ Check If The String Is A Palindrome/Solution.cpp
@@ -0,0 +1,29 @@
+char lower(char ch){
+    if((ch >= 'a' && ch <= 'z') || (ch >= 0 && ch <= 9)) return ch;
+    else{
+        ch = ch - 'A' + 'a';
+        return ch;
+    }
+}
+
+bool checkPalindrome(string s)
+{
+ int st=0; 
+    int e= s.size()-1;
+    while(st<=e){
+        if(!isalnum(s[st])){
+            st++;
+        }
+        else if(!isalnum(s[e])){
+            e--;
+        }
+        else if(lower(s[st])==lower(s[e])){
+            st++;
+            e--;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
