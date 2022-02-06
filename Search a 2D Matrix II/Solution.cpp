42f9a3391341849640cb476b090db93cb9fefbf6
Vartika Yaduvanshi <vv1415@srmist.edu.in>
2022-02-06 17:15:45 +0530

    Solution

---
+++ Search a 2D Matrix II/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+      int row = matrix.size();
+      int col= matrix[0].size();
+   
+      int rowIndex=0;
+      int colIndex= col-1;
+      while(rowIndex<row && colIndex>=0){
+        int element = matrix[rowIndex][colIndex];
+        if(element==target){
+          return 1;
+        }
+        else if(element<target){
+          rowIndex++;
+        }
+        else{
+          colIndex--;
+        }
+      }
+      return 0;
+    }
+};
