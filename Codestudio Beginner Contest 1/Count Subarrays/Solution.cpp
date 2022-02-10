1ff5904120557dc667fc941ffe5e5322518ee423
Vartika Yaduvanshi <vv1415@srmist.edu.in>
2022-02-10 22:53:25 +0530

    Solution

---
+++ Codestudio Beginner Contest 1/Count Subarrays/Solution.cpp
@@ -0,0 +1,17 @@
+int numberOfSubarrays(int n, vector<int> arr)
+{
+    int count =0;
+   for(int i=0;i<n-1;i++){
+       if(arr[i]==0){
+           if(arr[i+1]==1){
+               count++;
+           }
+       }
+       else if(arr[i]==1){
+           if(arr[i+1]==0){
+               count++;
+           }
+       }
+   }
+    return count;
+}
